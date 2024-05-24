155fa2d86ab7c4742aebcb9874b5e81b0409f98a
Milton <miltonio94@gmail.com>
2024-05-24 09:50:41 +0100

    Trying to figure out boolean logic and failing

---
+++ Notes/test_bool_logic.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+unsigned char test_bool_logic(unsigned char x, unsigned char y, unsigned char z) {
+  return  (~x  | ~z) & (y | x) & (y | ~z) & ~z ;
+      //(~x & y & ~z) | (x & ~z);
+}
+
+unsigned char original_bool_logic(unsigned char x, unsigned char y, unsigned char z) {
+  return (~x & y & ~z) | (x & ~y & ~z) | (x & y & ~z);
+}
+
+int main() {
+
+  printf("Original bool logic \n");
+  printf("%.2x\n", original_bool_logic(0, 0, 0));
+  printf("%.2x\n", original_bool_logic(0, 0, 255));
+  printf("%.2x\n", original_bool_logic(0, 255, 0));
+  printf("%.2x\n", original_bool_logic(0, 255, 255));
+  printf("%.2x\n", original_bool_logic(255, 0, 0));
+  printf("%.2x\n", original_bool_logic(255, 255, 0));
+  printf("%.2x\n", original_bool_logic(255, 255, 255));
+
+  printf("\n\n");
+  printf("Simplified bool logic \n");
+  printf("%.2x\n", test_bool_logic(0, 0, 0));
+  printf("%.2x\n", test_bool_logic(0, 0, 255));
+  printf("%.2x\n", test_bool_logic(0, 255, 0));
+  printf("%.2x\n", test_bool_logic(0, 255, 255));
+  printf("%.2x\n", test_bool_logic(255, 0, 0));
+  printf("%.2x\n", test_bool_logic(255, 255, 0));
+  printf("%.2x\n", test_bool_logic(255, 255, 255));
+}
